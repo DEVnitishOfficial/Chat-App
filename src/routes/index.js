b0d81e671cb3293a4bbafe543cc47c1aa76f2c78
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-04-26 23:59:25 +0530

    added user profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,7 +42,8 @@ export default function Router() {
         { path: "404", element: <Page404 /> },
         { path: "group", element: <GroupPage /> },
         {path: "call", element: <CallPage />},
-        { path: "*", element: <Navigate to="/404" replace /> },
+        {path: "profile", element: <ProfilePage />},
+        { path: "*", element: <Navigate to="/404" replace />},
       ],
     },
     { path: "*", element: <Navigate to="/404" replace /> },
@@ -58,6 +59,7 @@ const Settings = Loadable(
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
 const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")))
+const ProfilePage = Loadable(lazy(() => import("../pages/dashboard/Profile")))
 
 const LoginPage = Loadable(
   lazy(() => import("../pages/auth/Login"))
---
+++ src/sections/settings/ProfileFrom.js
@@ -0,0 +1,77 @@
+import React, { useCallback, useState } from "react";
+import * as Yup from "yup";
+// import { object, string, number, date, InferType } from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { Stack, Alert } from "@mui/material";
+import { RHFTextField } from "../../components/hook-form";
+
+const ProfileForm = () => {
+  const ProfileFromSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    about: Yup.string().required("About is required"),
+    avatarUrl: Yup.string().required("avatar"),
+  });
+
+  const defaultValues = {
+    name: "",
+    about: "",
+  };
+  const methods = useForm({
+    resolver: yupResolver(ProfileFromSchema),
+    defaultValues,
+  });
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const values = watch();
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue("avatarUrl",newFile,{shouldValidate:true});
+      }
+    },
+    [setValue]
+  );
+
+  const onSubmit = async (data) => {
+    try {
+      console.log("Data", data);
+    } catch (error) {
+      console.log("error from react from submitting>>", error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && <Alert>{errors.afterSubmit.message}</Alert>}
+
+        <RHFTextField name="name" label="Name" helperText={"This name will be visible to your contact"} />
+        
+        <RHFTextField multiline rows={4} maxRows={5} name="about" label="about" />
+      </Stack> 
+    </FormProvider>
+  );
+};
+
+export default ProfileForm;
